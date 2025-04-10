2ad1a2eeaee8b1bb060fea93575e9787d3ca304c
jiporCK <srengchipor99@gmail.com>
2025-04-10 14:38:52 +0700

    modified env

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+pg_user = os.getenv("PG_USERNAME")
+pg_pass = os.getenv("PG_PASSWORD")
+pg_db = os.getenv("PG_DB")
+pg_port = os.getenv("PG_PORT")
+pg_host = os.getenv("PG_HOST")
+
 # Database URL for PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:adminpw123@localhost:5432/fastapi_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
 
 # Create SQLAlchemy engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
